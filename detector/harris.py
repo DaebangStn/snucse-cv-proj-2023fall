995f48b7817d91f60d9b8f54dbcdef8596ad12b2
daebangstn <gunho1107@naver.com>
2023-10-28 08:47:53 +0900

    Harris Corner Detection with SubPixel Accuracy

--- detector/harris.py
+++ detector/harris.py
@@ -11,14 +11,19 @@ class Harris(BaseDetector):
 
     def detect(self, **kwargs):
         """
+        Harris Corner Detection with SubPixel Accuracy
+        refer to https://docs.opencv.org/3.4/dc/d0d/tutorial_py_features_harris.html
         :param kwargs:
         :return: list of points (x, y)
         """
         dst = cv2.cornerHarris(self._image, 5, 11, 0.04)
         dst = cv2.dilate(dst, None)
-        spot = dst > 0.05 * dst.max()
-        coordinates = np.where(spot)
-        points = list(zip(coordinates[1], coordinates[0]))
+        ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
+        dst = np.uint8(dst)
+        ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+        corners = cv2.cornerSubPix(self._image, np.float32(centroids), (5, 5), (-1, -1), criteria)
+        points = [(int(x), int(y)) for x, y in corners]
         return points
 
     def get_type(self) -> FeatureType:
